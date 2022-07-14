c8ec2fc0dbdb58e5b8b0a66f1a19de0cc6e59fed
Maria Suarez <nakanohalina@gmail.com>
2022-07-14 15:58:42 -0500

    Se agrega Widgets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
+import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Twitter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello World</h1>
+
+      <main>
+        <Sidebar />
+
+        <Feed />
+
+        <Widgets />
+      </main>
+
     </div>
   )
 }
